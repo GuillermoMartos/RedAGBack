d2d1af8f8d97f420e40b034f52087147c9679da5
GuillermoMartos <guille.l.martos@gmail.com>
2022-06-26 12:01:33 -0300

    env port

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-// require("dotenv").config();
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 let sequelize =
   process.env.NODE_ENV === "production"
@@ -27,7 +27,7 @@ let sequelize =
       },
       ssl: true,
     })
-    : new Sequelize(`postgres://postgres:superperro1!@localhost/redag`, {
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     });
