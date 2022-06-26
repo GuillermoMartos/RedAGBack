00afed080a5e3da594bdc8b2b30d7ab410d925d9
GuillermoMartos <guille.l.martos@gmail.com>
2022-06-26 12:41:33 -0300

    try

--- db.js
+++ db.js
@@ -3,14 +3,14 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-
+const port = process.env.PORT || 3001;
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
       database: DB_NAME,
       dialect: "postgres",
       host: DB_HOST,
-      port: 5432,
+      port: port,
       username: DB_USER,
       password: DB_PASSWORD,
       pool: {
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const server = require('./app');
 const { conn, Categoria } = require('./db');
-
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 //si pongo false en force, van quedando los datos en la db
 //para correr el back junto con la api, podemos poner este puerto en 3002, p.e.
 conn.sync({ force: false }).then(async () => {
-  server.listen(process.env.PORT, async () => {
-    console.log(process.env.PORT); // eslint-disable-line no-console
+  server.listen(port, async () => {
+    console.log(port); // eslint-disable-line no-console
   });
 
 
