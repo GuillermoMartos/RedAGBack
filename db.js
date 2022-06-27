1e5f3e54b5b79cb4847009914ab2131265dacfb7
GuillermoMartos <guille.l.martos@gmail.com>
2022-06-27 00:17:16 -0300

    procfile

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 console.log('soy el port', port)
 let sequelize =
   process.env.NODE_ENV === "production"
