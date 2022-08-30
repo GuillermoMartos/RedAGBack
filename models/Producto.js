aae046cf0eade7eb1fe14e1126317a698c709676
GuillermoMartos <guille.l.martos@gmail.com>
2022-08-30 01:24:48 -0300

    imagenes text

--- models/Producto.js
+++ models/Producto.js
@@ -26,7 +26,7 @@ module.exports = (sequelize) => {
       allowNull: false
     },
     imagen: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: true
     },
     marca: {
