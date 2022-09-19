77c49059b20976ff845a8b4661f678479a5aa60e
GuillermoMartos <guille.l.martos@gmail.com>
2022-09-19 00:59:51 -0300

    cambio base de datos disponible

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const server = require('./app');
-const { conn, Categoria } = require('./db');
+const { conn, Categoria, Admin } = require('./db');
 const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 //si pongo false en force, van quedando los datos en la db
 //para correr el back junto con la api, podemos poner este puerto en 3002, p.e.
-conn.sync({ force: true }).then(async () => {
+conn.sync({ force: false }).then(async () => {
   server.listen(port, async () => {
     console.log(port); // eslint-disable-line no-console
   });
 
+  await Admin.create({ email: 'guille.l.martos@gmail.com' })
 
-  let categoriasIniciales = ['Quesos', 'Dulces', 'Fiambres', 'Lácteos', 'Salsas', 'Picantes', 'Huevos', 'Aceites', 'Sales', 'Hongos', 'Comidas veganas', 'Pan y Masas', 'Legumbres', 'Baño y Limpieza', 'Plantas', 'Verduras', 'Otros']
 
-  const crear = await Categoria.bulkCreate(categoriasIniciales.map(d => {
-    return {
-      nombre: d,
-    }
-  }))
+  //esta funcion comentada es para crear todas las categorias
+  // let categoriasIniciales = ['Quesos', 'Dulces', 'Fiambres', 'Lácteos', 'Salsas', 'Picantes', 'Huevos', 'Aceites', 'Sales', 'Hongos', 'Comidas veganas', 'Pan y Masas', 'Legumbres', 'Baño y Limpieza', 'Plantas', 'Verduras', 'Otros']
+
+  // const crear = await Categoria.bulkCreate(categoriasIniciales.map(d => {
+  //   return {
+  //     nombre: d,
+  //   }
+  // }))
 });
